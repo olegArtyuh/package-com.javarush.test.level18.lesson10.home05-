0b5fbd5ba57e07ef4a1e15863b2ee6c5692102ce
olegArtyuh <oleg.o.artyuh@gmail.com>
2016-03-22 01:21:23 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,5 +1,8 @@
 package com.javarush.test.level18.lesson10.home05
 
+import java.io.*;
+import java.util.Scanner;
+
 /* Округление чисел
 Считать с консоли 2 имени файла
 Первый файл содержит вещественные(дробные) числа, разделенные пробелом. Например, 3.1415
@@ -13,9 +16,6 @@ package com.javarush.test.level18.lesson10.home05
 -3.50 - -3
 -3.51 - -4
 */
-
-import java.io.*;
-import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws IOException
